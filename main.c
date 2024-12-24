0dd9da0b110e79f5f39373486ac5ffd5909d6c2b
TahaNour <tahanour.chabchoub@supcom.tn>
2024-12-24 16:57:54 +0100

    edit the test

--- main.c
+++ main.c
@@ -8,10 +8,12 @@
 #define MULTIPLIER 31ULL
 
 // Fonction de hashage
-unsigned long long f_hashage(char *input) {
+unsigned long long f_hashage(char *input)
+{
     unsigned long long hash = 0;
     int i = 0;
-    while (input[i] != '\0') {
+    while (input[i] != '\0')
+    {
         hash = (hash * MULTIPLIER + input[i]) % MOD;
         i++;
     }
@@ -19,20 +21,23 @@ unsigned long long f_hashage(char *input) {
 }
 
 // Conversion en Base62
-void To_Base62(unsigned long long hash, char *output) {
+void To_Base62(unsigned long long hash, char *output)
+{
     const char alphabets[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    strcpy(output, "000000");  // Initialisation de output
+    strcpy(output, "000000"); // Initialisation de output
     int i = -1;
-    do {
+    do
+    {
         i++;
         output[i] = alphabets[hash % 62];
         hash /= 62;
     } while (hash > 0);
-    output[i + 1] = '\0';  // Assure la terminaison de la chaîne
+    output[i + 1] = '\0'; // Assure la terminaison de la chaîne
 }
 
 // Fonction de génération du code (encode)
-void encode(char *input, char *output) {
+void encode(char *input, char *output)
+{
     unsigned long long hash = f_hashage(input);
     char output_inter[7];
     To_Base62(hash, output_inter);
@@ -40,12 +45,14 @@ void encode(char *input, char *output) {
 }
 
 // Fonction de vérification de l'existence du long_url dans la base
-int check_url_in_db(sqlite3 *db, const char *long_url, int *num, char *code) {
+int check_url_in_db(sqlite3 *db, const char *long_url, int *num, char *code)
+{
     sqlite3_stmt *stmt;
     const char *sql = "SELECT num, code FROM urls WHERE long_url = ?";
     int rc = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
 
-    if (rc != SQLITE_OK) {
+    if (rc != SQLITE_OK)
+    {
         printf("Erreur de préparation de la requête : %s\n", sqlite3_errmsg(db));
         return 0;
     }
@@ -53,7 +60,8 @@ int check_url_in_db(sqlite3 *db, const char *long_url, int *num, char *code) {
     sqlite3_bind_text(stmt, 1, long_url, -1, SQLITE_STATIC);
 
     rc = sqlite3_step(stmt);
-    if (rc == SQLITE_ROW) {
+    if (rc == SQLITE_ROW)
+    {
         *num = sqlite3_column_int(stmt, 0);
         strcpy(code, (const char *)sqlite3_column_text(stmt, 1));
         sqlite3_finalize(stmt);
@@ -65,12 +73,14 @@ int check_url_in_db(sqlite3 *db, const char *long_url, int *num, char *code) {
 }
 
 // Fonction pour insérer un nouveau couple (num, code, long_url)
-int insert_url(sqlite3 *db, int num, const char *code, const char *long_url) {
+int insert_url(sqlite3 *db, int num, const char *code, const char *long_url)
+{
     sqlite3_stmt *stmt;
     const char *sql = "INSERT INTO urls (num, code, long_url) VALUES (?, ?, ?)";
     int rc = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
 
-    if (rc != SQLITE_OK) {
+    if (rc != SQLITE_OK)
+    {
         printf("Erreur de préparation de la requête : %s\n", sqlite3_errmsg(db));
         return 0;
     }
@@ -80,7 +90,8 @@ int insert_url(sqlite3 *db, int num, const char *code, const char *long_url) {
     sqlite3_bind_text(stmt, 3, long_url, -1, SQLITE_STATIC);
 
     rc = sqlite3_step(stmt);
-    if (rc != SQLITE_DONE) {
+    if (rc != SQLITE_DONE)
+    {
         printf("Erreur lors de l'insertion : %s\n", sqlite3_errmsg(db));
         sqlite3_finalize(stmt);
         return 0;
@@ -91,12 +102,14 @@ int insert_url(sqlite3 *db, int num, const char *code, const char *long_url) {
 }
 
 // Fonction pour obtenir le maximum de `num` pour un `code` donné
-int get_max_num_for_code(sqlite3 *db, const char *code) {
+int get_max_num_for_code(sqlite3 *db, const char *code)
+{
     sqlite3_stmt *stmt;
     const char *sql = "SELECT MAX(num) FROM urls WHERE code = ?";
     int rc = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
 
-    if (rc != SQLITE_OK) {
+    if (rc != SQLITE_OK)
+    {
         printf("Erreur de préparation de la requête : %s\n", sqlite3_errmsg(db));
         return -1;
     }
@@ -104,7 +117,8 @@ int get_max_num_for_code(sqlite3 *db, const char *code) {
     sqlite3_bind_text(stmt, 1, code, -1, SQLITE_STATIC);
 
     rc = sqlite3_step(stmt);
-    if (rc == SQLITE_ROW) {
+    if (rc == SQLITE_ROW)
+    {
         int max_num = sqlite3_column_int(stmt, 0);
         sqlite3_finalize(stmt);
         return max_num;
@@ -115,7 +129,8 @@ int get_max_num_for_code(sqlite3 *db, const char *code) {
 }
 
 // Fonction de décodage : "num-code" -> long_url
-int decode(sqlite3 *db, const char *short_url, char *long_url) {
+int decode(sqlite3 *db, const char *short_url, char *long_url)
+{
     int num;
     char code[7];
     sscanf(short_url, "%d-%s", &num, code);
@@ -124,7 +139,8 @@ int decode(sqlite3 *db, const char *short_url, char *long_url) {
     const char *sql = "SELECT long_url FROM urls WHERE num = ? AND code = ?";
     int rc = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
 
-    if (rc != SQLITE_OK) {
+    if (rc != SQLITE_OK)
+    {
         printf("Erreur de préparation de la requête : %s\n", sqlite3_errmsg(db));
         return 0;
     }
@@ -133,7 +149,8 @@ int decode(sqlite3 *db, const char *short_url, char *long_url) {
     sqlite3_bind_text(stmt, 2, code, -1, SQLITE_STATIC);
 
     rc = sqlite3_step(stmt);
-    if (rc == SQLITE_ROW) {
+    if (rc == SQLITE_ROW)
+    {
         strcpy(long_url, (const char *)sqlite3_column_text(stmt, 0));
         sqlite3_finalize(stmt);
         return 1; // Long URL trouvé
@@ -143,13 +160,15 @@ int decode(sqlite3 *db, const char *short_url, char *long_url) {
     return 0; // Long URL non trouvé
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     sqlite3 *db;
     char *err_msg = 0;
     int rc;
 
     // Vérifier les arguments passés
-    if (argc < 2) {
+    if (argc < 2)
+    {
         printf("Usage: %s <option> [paramètres]\n", argv[0]);
         printf("Options:\n");
         printf("  encode <long_url>    : Générer un short_url\n");
@@ -159,13 +178,16 @@ int main(int argc, char *argv[]) {
 
     // Ouvrir la base de données
     rc = sqlite3_open("short.db", &db);
-    if (rc != SQLITE_OK) {
+    if (rc != SQLITE_OK)
+    {
         fprintf(stderr, "Impossible d'ouvrir la base de données : %s\n", sqlite3_errmsg(db));
         return 1;
     }
 
-    if (strcmp(argv[1], "encode") == 0) {
-        if (argc < 3) {
+    if (strcmp(argv[1], "encode") == 0)
+    {
+        if (argc < 3)
+        {
             printf("Veuillez fournir un long_url à encoder.\n");
             sqlite3_close(db);
             return 1;
@@ -176,29 +198,41 @@ int main(int argc, char *argv[]) {
         char code[7];
 
         // Vérifier si le long_url est déjà dans la base de données
-        if (check_url_in_db(db, long_url, &num, code)) {
+        if (check_url_in_db(db, long_url, &num, code))
+        {
             printf("short url correspond = %d-%s\n", num, code);
-        } else {
+        }
+        else
+        {
             // URL non trouvée, générer un code
             encode(long_url, code);
 
             // Vérifier si le code existe déjà dans la base
             int max_num = get_max_num_for_code(db, code);
-            if (max_num >= 0) {
+            if (max_num >= 0)
+            {
                 num = max_num + 1; // Incrémenter le num pour ce code
-            } else {
+            }
+            else
+            {
                 num = 1; // Premier numéro pour ce code
             }
 
             // Insérer le nouveau couple (num, code, long_url) dans la base
-            if (insert_url(db, num, code, long_url)) {
+            if (insert_url(db, num, code, long_url))
+            {
                 printf("short url correspond = %d-%s\n", num, code);
-            } else {
+            }
+            else
+            {
                 printf("Erreur lors de l'insertion.\n");
             }
         }
-    } else if (strcmp(argv[1], "decode") == 0) {
-        if (argc < 3) {
+    }
+    else if (strcmp(argv[1], "decode") == 0)
+    {
+        if (argc < 3)
+        {
             printf("Veuillez fournir un short_url à décoder.\n");
             sqlite3_close(db);
             return 1;
@@ -207,12 +241,17 @@ int main(int argc, char *argv[]) {
         char *short_url = argv[2];
         char decoded_url[256];
 
-        if (decode(db, short_url, decoded_url)) {
+        if (decode(db, short_url, decoded_url))
+        {
             printf("long url correspond = %s\n", decoded_url);
-        } else {
+        }
+        else
+        {
             printf("long_url n'est pas trouvé\n");
         }
-    } else {
+    }
+    else
+    {
         printf("Option invalide. Utilisez 'encode' ou 'decode'.\n");
     }
 
